e4cdccc47c119ad5f80ad18c1a24f1bc54780f1b
Andrew <imozzart1@gmail.com>
2023-03-15 18:05:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 export const App = () => {
-	const [done, setDone] = useState(false);
-	const [counter, setCounter] = useState(0);
+  const [done, setDone] = useState(false);
+  const [counter, setCounter] = useState(0);
 
-	useLayoutEffect(() => {
-		if (counter < 20) {
-			setCounter(counter + 1);	
-		}
-	}) // intentionally run on every render
+  useLayoutEffect(() => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  }); // intentionally run on every render
 
-	useEffect(() => {
-		setDone(true);
-	}) // intentionally run on every render
+  useEffect(() => {
+    setDone(true);
+  }); // intentionally run on every render
 
-	console.log("render done", done);
-	console.log("render counter", counter);
-	console.log("render\n");
+  console.log("render done", done);
+  console.log("render counter", counter);
+  console.log("render\n");
 
-	return (
-		<div>{counter} renders</div>
-	);
-}
+  return null;
+};
